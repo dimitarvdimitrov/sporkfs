64597abf373edc070dab0d583bad15f816301109
Dimitar Dimitrov <donkorleone20@gmail.com>
2019-12-01 14:34:38 +0000

    Fix raft index to return multiple peers based on redundancy

--- raft/index/index.go
+++ raft/index/index.go
@@ -1,13 +1,24 @@
 package index
 
+const (
+	redundancy = 2
+)
+
 var (
 	minusOne = -1
 	maxId    = uint64(minusOne)
 )
 
-func FindPeer(peerList []string, fileId uint64) string {
+func FindPeers(peerList []string, fileId uint64) []string {
 	numPeers := uint64(len(peerList))
-	perBucket := maxId / numPeers
-	peerIdx := (fileId / perBucket) % numPeers
-	return peerList[peerIdx]
+
+	idsPerPeer := maxId / numPeers
+	peersWithFile := make([]string, 0, redundancy)
+
+	for i := uint64(0); i < redundancy; i++ {
+		peerIdx := (i + fileId/idsPerPeer) % numPeers
+		peersWithFile = append(peersWithFile, peerList[peerIdx])
+	}
+
+	return peersWithFile
 }
