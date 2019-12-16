1e9d9f269bac9391dfdedb19fc520bb1873dcead
Dimitar Dimitrov <donkorleone20@gmail.com>
2019-12-16 16:18:04 +0000

    Change interface of gRPC File service

--- api/file.go
+++ api/file.go
@@ -1,7 +1,11 @@
 package api
 
 import (
+	"io"
+	"os"
+
 	proto "github.com/dimitarvdimitrov/sporkfs/api/pb"
+	"github.com/dimitarvdimitrov/sporkfs/log"
 	"github.com/dimitarvdimitrov/sporkfs/store/data"
 )
 
@@ -20,34 +24,37 @@ func NewFileServer(s data.Driver) *fileServer {
 }
 
 func (server *fileServer) Read(req *proto.ReadRequest, stream proto.File_ReadServer) error {
-	//ctx := stream.Context()
+	log.Debug("received request for %d-%d", req.Id, req.Version)
+	reader, err := server.data.Reader(req.Id, req.Version, os.O_RDONLY)
+	if err != nil {
+		return err
+	}
 
-	//bytes, err := server.data.Read(req.Id, req.Version, req.Offset, req.Size)
-	//if err != nil && err != io.EOF {
-	//	return err
-	//}
-	//
-	//for len(bytes) > 0 {
-	//	select {
-	//	case <-ctx.Done():
-	//		return ctx.Err()
-	//	default:
-	//	}
-	//
-	//	lenToSend := min(len(bytes), ChunkSize)
-	//	toSend := bytes[:lenToSend]
-	//	msg := &proto.ReadReply{
-	//		Content: toSend,
-	//	}
-	//	if err = stream.Send(msg); err != nil {
-	//		return err
-	//	}
-	//	bytes = bytes[lenToSend:]
-	//}
+	off := int64(0)
+	buff := make([]byte, ChunkSize, ChunkSize)
+
+	for {
+		n, err := reader.ReadAt(buff, off)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if n < 1 {
+			break
+		}
+
+		msg := &proto.ReadReply{
+			Content: buff[:n],
+		}
+		if err = stream.Send(msg); err != nil {
+			return err
+		}
+		off += int64(n)
+		buff = buff[:cap(buff)] // reset the buffer
+	}
 	return nil
 }
 
-func min(a, b int) int {
+func min(a, b int64) int64 {
 	if a > b {
 		return b
 	}
